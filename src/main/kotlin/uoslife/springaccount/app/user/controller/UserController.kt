e3be647e0ed94fc35e009d57d3d3663ff05d6b27
23tae <taehoonkim.dev@gmail.com>
2024-08-13 19:26:13 +0900

    chore: 의존성 주입

--- src/main/kotlin/uoslife/springaccount/app/user/controller/UserController.kt
+++ src/main/kotlin/uoslife/springaccount/app/user/controller/UserController.kt
@@ -1,7 +1,8 @@
 package uoslife.springaccount.app.user.controller
 
 import org.springframework.web.bind.annotation.RestController
+import uoslife.springaccount.app.user.service.UserService
 
 @RestController
-class UserController {
+class UserController(private val userService: UserService) {
 }
