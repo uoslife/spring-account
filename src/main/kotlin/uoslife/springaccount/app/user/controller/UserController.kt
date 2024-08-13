b8ad1fa6a37ddd9621bb3f2b9eaa561e9e7214e0
23tae <taehoonkim.dev@gmail.com>
2024-08-13 19:18:35 +0900

    feat: controller, service 생성

---
+++ src/main/kotlin/uoslife/springaccount/app/user/controller/UserController.kt
@@ -0,0 +1,7 @@
+package uoslife.springaccount.app.user.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserController {
+}
---
+++ src/main/kotlin/uoslife/springaccount/app/user/service/UserService.kt
@@ -0,0 +1,7 @@
+package uoslife.springaccount.app.user.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class UserService {
+}
