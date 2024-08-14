8e0d1cd49c5d52d31593948f6dd5c65979aad6ff
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2024-08-14 17:29:51 +0900

    feat: add redis settings using redisson

---
+++ src/main/kotlin/uoslife/springaccount/common/domain/config/RedisConfig.kt
@@ -0,0 +1,22 @@
+package uoslife.springaccount.common.domain.config
+
+import org.redisson.Redisson
+import org.redisson.api.RedissonClient
+import org.redisson.config.Config
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class RedisConfig(
+    @Value("\${spring.data.redis.host}") val host: String,
+    @Value("\${spring.data.redis.port}") val port: Int,
+) {
+
+    @Bean
+    fun redissonClient(): RedissonClient {
+        val config = Config()
+        config.useSingleServer().address = "redis://$host:$port"
+        return Redisson.create(config)
+    }
+}
