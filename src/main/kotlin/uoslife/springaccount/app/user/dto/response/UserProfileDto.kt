2cd815d3d4ab708ff5b8ea4a0ed240d06204fa27
23tae <taehoonkim.dev@gmail.com>
2024-08-21 00:18:28 +0900

    chore: 응답용 유저 정보 dto

---
+++ src/main/kotlin/uoslife/springaccount/app/user/dto/response/UserProfileDto.kt
@@ -0,0 +1,38 @@
+package uoslife.springaccount.app.user.dto.response
+
+class UserProfileDto {
+
+    data class UserRealm(
+        val code: String,
+        val name: String,
+    )
+
+    data class UserProfileIdentity(
+        val id: String,
+        val type: String,
+        val status: String,
+        val idNumber: String,
+        val university: String?,
+        val department: String?,
+        val major: String?,
+    )
+
+    data class UserProfileModerator(
+        val generation: String,
+        val chapter: String,
+        val role: String,
+    )
+
+    data class UserProfileResponse(
+        val id: Long,
+        val nickname: String,
+        val phone: String,
+        val name: String?,
+        val email: String?,
+        val realm: UserRealm?,
+        val identity: UserProfileIdentity?,
+        val moderator: UserProfileModerator?,
+        val isLinkedPortal: Boolean,
+        val isVerified: Boolean,
+    )
+}
