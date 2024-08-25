01dffdd8857032f355b2f2dc8d9c219df6adf6f0
23tae <taehoonkim.dev@gmail.com>
2024-08-25 23:38:04 +0900

    feat: id로 사용자를 찾는 메서드 정의

--- src/main/kotlin/uoslife/springaccount/app/user/domain/repository/jpa/UserRepository.kt
+++ src/main/kotlin/uoslife/springaccount/app/user/domain/repository/jpa/UserRepository.kt
@@ -2,5 +2,8 @@ package uoslife.springaccount.app.user.domain.repository.jpa
 
 import org.springframework.data.jpa.repository.JpaRepository
 import uoslife.springaccount.app.user.domain.entity.User
+import java.util.*
 
-interface UserRepository : JpaRepository<User, Long>
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByIdAndDeletedAtIsNull(id: Long): Optional<User>
+}
