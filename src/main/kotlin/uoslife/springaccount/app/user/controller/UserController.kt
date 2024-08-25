00a94e25c27dae73c871fac8ca069db73c70d0ec
23tae <taehoonkim.dev@gmail.com>
2024-08-25 13:47:33 +0900

    feat: 자신의 프로필 정보 조회

--- src/main/kotlin/uoslife/springaccount/app/user/controller/UserController.kt
+++ src/main/kotlin/uoslife/springaccount/app/user/controller/UserController.kt
@@ -1,8 +1,16 @@
 package uoslife.springaccount.app.user.controller
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import uoslife.springaccount.app.user.service.UserService
 
 @RestController
 class UserController(private val userService: UserService) {
+
+    @GetMapping("/me")
+    fun getMyProfile(@AuthenticationPrincipal userDetails: UserDetails) {
+        val userId = userDetails.username.toLong()
+    }
 }
