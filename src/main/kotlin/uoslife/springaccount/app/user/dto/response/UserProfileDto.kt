542ddd9c669ce0a3409c0288f3213053302b7eb6
23tae <taehoonkim.dev@gmail.com>
2024-08-27 00:38:13 +0900

    feat: user entity를 Dto에 매핑

--- src/main/kotlin/uoslife/springaccount/app/user/dto/response/UserProfileDto.kt
+++ src/main/kotlin/uoslife/springaccount/app/user/dto/response/UserProfileDto.kt
@@ -1,6 +1,9 @@
 package uoslife.springaccount.app.user.dto.response
 
+import uoslife.springaccount.app.user.domain.entity.User
 import uoslife.springaccount.app.verification.domain.entity.VerificationMethod
+import uoslife.springaccount.common.error.ErrorCode
+import uoslife.springaccount.common.error.baseexception.BusinessException
 
 class UserProfileDto {
 
@@ -31,11 +34,46 @@ class UserProfileDto {
         val phone: String,
         val name: String?,
         val email: String?,
-        val realm: UserRealm?,
+        //        val realm: UserRealm?,
         val identity: UserProfileIdentity?,
         val moderator: UserProfileModerator?,
         val isLinkedPortal: Boolean,
         val isVerified: Boolean,
         val verificationMethod: VerificationMethod?,
     )
+
+    companion object {
+        fun toUserProfileResponse(user: User): UserProfileResponse {
+            return UserProfileResponse(
+                id = user.id ?: throw BusinessException(ErrorCode.USER_ID_NULL),
+                nickname = user.nickname ?: "No Nickname",
+                phone = user.phoneNumber,
+                name = user.name,
+                email = user.email,
+                identity =
+                    user.identities.firstOrNull()?.let { identity ->
+                        UserProfileIdentity(
+                            id = identity.id,
+                            type = identity.type,
+                            status = identity.status,
+                            idNumber = identity.idNumber,
+                            university = identity.university,
+                            department = identity.department,
+                            major = identity.major
+                        )
+                    },
+                moderator =
+                    user.moderators.firstOrNull()?.let {
+                        UserProfileModerator(
+                            generation = it.generation,
+                            chapter = it.chapter,
+                            role = it.role
+                        )
+                    },
+                isLinkedPortal = user.portalAccounts.isNotEmpty(),
+                isVerified = user.verifications.isNotEmpty(),
+                verificationMethod = user.verifications.firstOrNull()?.method
+            )
+        }
+    }
 }
