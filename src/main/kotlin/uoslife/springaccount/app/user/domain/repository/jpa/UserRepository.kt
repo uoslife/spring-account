4e54a5da00b46121a1234619650c558b8f965272
23tae <taehoonkim.dev@gmail.com>
2024-08-27 00:37:02 +0900

    chore: jpa 반환값 수정

--- src/main/kotlin/uoslife/springaccount/app/user/domain/repository/jpa/UserRepository.kt
+++ src/main/kotlin/uoslife/springaccount/app/user/domain/repository/jpa/UserRepository.kt
@@ -2,8 +2,7 @@ package uoslife.springaccount.app.user.domain.repository.jpa
 
 import org.springframework.data.jpa.repository.JpaRepository
 import uoslife.springaccount.app.user.domain.entity.User
-import java.util.*
 
 interface UserRepository : JpaRepository<User, Long> {
-    fun findByIdAndDeletedAtIsNull(id: Long): Optional<User>
+    fun findByIdAndDeletedAtIsNull(id: Long): User?
 }
