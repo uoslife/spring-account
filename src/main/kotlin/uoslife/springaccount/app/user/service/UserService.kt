5cf6355d9182defdc3b43c50e3ca501edf5a9a99
23tae <taehoonkim.dev@gmail.com>
2024-08-27 00:36:24 +0900

    feat: 유저 프로필 조회 비즈니스 로직

--- src/main/kotlin/uoslife/springaccount/app/user/service/UserService.kt
+++ src/main/kotlin/uoslife/springaccount/app/user/service/UserService.kt
@@ -1,7 +1,36 @@
 package uoslife.springaccount.app.user.service
 
+import org.springframework.cache.CacheManager
 import org.springframework.stereotype.Service
+import uoslife.springaccount.app.user.domain.repository.jpa.UserRepository
+import uoslife.springaccount.app.user.dto.response.UserProfileDto
+import uoslife.springaccount.common.error.user.UserNotFoundException
 
 @Service
-class UserService {
+class UserService(
+    private val userRepository: UserRepository,
+    private val cacheManager: CacheManager,
+) {
+    fun getProfile(userId: Long): UserProfileDto.UserProfileResponse {
+        val cacheKey = "uoslife:user:profile:$userId"
+        val cache = cacheManager.getCache("userProfileCache")
+
+        // 캐시에서 데이터 가져오기
+        val cachedData = cache?.get(cacheKey, UserProfileDto.UserProfileResponse::class.java)
+        if (cachedData != null) {
+            return cachedData
+        }
+
+        // DB에서 유저 조회
+        val user =
+            userRepository.findByIdAndDeletedAtIsNull(userId) ?: throw UserNotFoundException()
+
+        // 유저 엔티티를 UserProfileResponse DTO로 변환
+        val userProfileResponse = UserProfileDto.toUserProfileResponse(user)
+
+        // 캐시에 저장
+        cache?.put(cacheKey, userProfileResponse)
+
+        return userProfileResponse
+    }
 }
