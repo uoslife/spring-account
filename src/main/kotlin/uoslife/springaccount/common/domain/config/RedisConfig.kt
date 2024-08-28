4027f523f0d9e6b3924804aadfe3faf9c4e42179
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2024-08-28 22:42:02 +0900

    feat: add codec config in redisson

--- src/main/kotlin/uoslife/springaccount/common/domain/config/RedisConfig.kt
+++ src/main/kotlin/uoslife/springaccount/common/domain/config/RedisConfig.kt
@@ -2,6 +2,7 @@ package uoslife.springaccount.common.domain.config
 
 import org.redisson.Redisson
 import org.redisson.api.RedissonClient
+import org.redisson.client.codec.StringCodec
 import org.redisson.config.Config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -17,6 +18,7 @@ class RedisConfig(
     fun redissonClient(): RedissonClient {
         val config = Config()
         config.useSingleServer().address = "redis://$host:$port"
+        config.setCodec(StringCodec.INSTANCE)
         return Redisson.create(config)
     }
 }
