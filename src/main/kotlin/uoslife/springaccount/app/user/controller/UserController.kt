f370afe9adddb1cdc71ccb0a5d4acda0c6c06589
23tae <taehoonkim.dev@gmail.com>
2024-08-31 22:38:01 +0900

    feat: 회원등록 컨트롤러 구현중

--- src/main/kotlin/uoslife/springaccount/app/user/controller/UserController.kt
+++ src/main/kotlin/uoslife/springaccount/app/user/controller/UserController.kt
@@ -1,12 +1,16 @@
 package uoslife.springaccount.app.user.controller
 
+import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import uoslife.springaccount.app.user.dto.param.CreateUserDto
+import uoslife.springaccount.app.user.dto.request.RegisterDto
 import uoslife.springaccount.app.user.dto.response.UserProfileDto
 import uoslife.springaccount.app.user.service.UserService
 
@@ -28,4 +32,22 @@ class UserController(private val userService: UserService) {
     ): ResponseEntity<UserProfileDto.UserProfileResponse> {
         return ResponseEntity.ok(userService.getProfile(userId))
     }
+
+    @PostMapping("/")
+    fun registerUser(
+        @AuthenticationPrincipal userDetails: UserDetails, // TODO register token 사용 방법 확인 필요
+        @Valid request: RegisterDto,
+    ) {
+        val data = CreateUserDto(
+            nickname = request.nickname,
+            phoneNumber = "01012345678", // TODO token에서 정보 가져오기
+        )
+
+        val user = userService.registerUser(data)
+    }
+
+    // TODO 토큰 발급
+
+    // TODO 토큰 반환
+
 }
