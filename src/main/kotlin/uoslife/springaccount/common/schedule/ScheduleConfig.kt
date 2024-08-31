0e531a3bb33d3943a1a7520d20a875891b988153
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2024-09-01 00:34:59 +0900

    feat: add ScheduleConfig

---
+++ src/main/kotlin/uoslife/springaccount/common/schedule/ScheduleConfig.kt
@@ -0,0 +1,6 @@
+package uoslife.springaccount.common.schedule
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration @EnableScheduling class ScheduleConfig {}
