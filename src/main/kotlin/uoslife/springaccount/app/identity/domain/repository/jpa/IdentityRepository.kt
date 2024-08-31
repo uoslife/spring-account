901a425fa92c1e16c694b3f519ce7f196ec32a08
youngchan Kim <aacz12035@gmail.com>
2024-09-01 01:12:52 +0900

    feat: repository IdentityList 업데이트 함수 추

--- src/main/kotlin/uoslife/springaccount/app/identity/domain/repository/jpa/IdentityRepository.kt
+++ src/main/kotlin/uoslife/springaccount/app/identity/domain/repository/jpa/IdentityRepository.kt
@@ -1,8 +1,15 @@
 package uoslife.springaccount.app.identity.domain.repository.jpa
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import uoslife.springaccount.app.identity.domain.entity.Identity
 
 interface IdentityRepository : JpaRepository<Identity, String> {
-    fun findIdentityListByUuid(uuid: String): List<Identity>?
+    fun findIdentitiesByUserId(userId: Long): List<Identity>?
+
+    // 추후 queryDsl로 변경 예정.
+    @Modifying
+    @Query("UPDATE Identity i SET i.isActive = CASE WHEN i.id = :identityId THEN true ELSE false END WHERE i.userId = :userId")
+    fun updateActiveIdentityList(identityId:String)
 }
